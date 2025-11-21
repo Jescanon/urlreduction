073bdab204cf10e4e7eb53395766a6dbd15a65cb
Jeca <smsacha2013@yandex.ru>
2025-11-21 22:03:24 +0700

    add database.py, requirements.txt, settings.py, and other

---
+++ src/database/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from src.head.settings import get_settings
+
+async_engine = create_async_engine(get_settings.database_url, echo=True)
+async_session = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
+class Base(DeclarativeBase):
+    pass
